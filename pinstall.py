2b9a8d28cb4923d8cab383f82b48b237cd0f8f06
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-09-13 13:48:30 +0600

    Update pinstall.py

--- pinstall.py
+++ pinstall.py
@@ -1,2 +1,21 @@
+#Programme By Mr. Beta
+#You Can Use It On Your Scrept
 import os
 from os import system as run
+py = 'python3 -m'
+
+def b_install(module):
+    while True:
+        try:
+            import module
+            break
+        except:
+            run(f'{py} pip install {module}')
+
+"""
+Uses:
+from pinstall import b_install as m_install
+m_install('requests')
+m_install('rich')
+
+"""
